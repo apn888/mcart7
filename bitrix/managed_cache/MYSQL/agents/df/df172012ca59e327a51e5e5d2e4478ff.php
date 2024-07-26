6163bffa2afbe7ef0b8610597c12c90fe8380d18
Art Pajgashov <apn888777@yandex.ru>
2024-07-26 15:05:33 +0300

    task170180 3  commit

--- bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
+++ bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001721994541';
-$dateexpire = '001721998201';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1721994542;}';
+$datecreate = '001721995501';
+$dateexpire = '001721999161';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1721995502;}';
 return true;
 ?>
