f59102bf9839763676bb04b95f56b3fefc0e0742
Art Pajgashov <apn888777@yandex.ru>
2024-08-12 21:44:47 +0300

    task 170188

--- index.php
+++ index.php
@@ -2,9 +2,73 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 ?>
-
-
-
+<?$GLOBALS['arrFilter'] = array('ACTIVE' => 'Y', '!PROPERTY_PRIORITY_DEAL' => false);?>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	".default", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "259 200",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "arrFilter",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "announcement",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "PRIORITY_DEAL",
+			1 => "",
+		),
+		"SET_BROWSER_TITLE" => "Y",
+		"PRIORITY_DEAL" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
   <div class="slide-one-item home-slider owl-carousel">
 
 	  <div class="site-blocks-cover" style="background-image: url(<?=SITE_TEMPLATE_PATH;?>/images/hero_bg_1.jpg);" data-aos="fade"
@@ -43,40 +107,89 @@ $APPLICATION->SetTitle("Мебельная компания");
 
       <div class="row">
         <div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
-          <div class="feature d-flex align-items-start">
-            <span class="icon mr-3 flaticon-house"></span>
-            <div class="text">
-              <h2 class="mt-0">Wide Range of Properties</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.
-              </p>
-            </div>
-          </div>
+
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                ".default",
+                array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/advantages1.php",
+                    "COMPONENT_TEMPLATE" => ".default"
+                ),
+                false
+            );?>
+
+
+
         </div>
         <div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
-          <div class="feature d-flex align-items-start">
-            <span class="icon mr-3 flaticon-rent"></span>
-            <div class="text">
-              <h2 class="mt-0">Rent or Sale</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.
-              </p>
-            </div>
-          </div>
+
+
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                ".default",
+                array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/advantages2.php",
+                    "COMPONENT_TEMPLATE" => ".default"
+                ),
+                false
+            );?>
+
         </div>
         <div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
-          <div class="feature d-flex align-items-start">
-            <span class="icon mr-3 flaticon-location"></span>
-            <div class="text">
-              <h2 class="mt-0">Property Location</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.
-              </p>
-            </div>
-          </div>
+
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                ".default",
+                array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/advantages3.php",
+                    "COMPONENT_TEMPLATE" => ".default"
+                ),
+                false
+            );?>
+
+
         </div>
       </div>
     </div>
   </div>
   <div class="site-section site-section-sm bg-light">
     <div class="container">
+
+        <?$APPLICATION->IncludeComponent(
+	"bitrix:news.line", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"IBLOCK_TYPE" => "announcement",
+		"IBLOCKS" => array(
+		),
+		"NEWS_COUNT" => "9",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"DETAIL_URL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "259 200",
+		"CACHE_GROUPS" => "Y",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y"
+	),
+	false
+);?>
+
       <div class="row mb-5">
         <div class="col-12">
           <div class="site-section-title">
@@ -301,6 +414,35 @@ $APPLICATION->SetTitle("Мебельная компания");
       <div class="row justify-content-center">
         <div class="col-md-7 text-center mb-5">
           <div class="site-section-title">
+
+              <br>
+              <?$APPLICATION->IncludeComponent(
+	"bitrix:news.line", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"IBLOCK_TYPE" => "siteservice",
+		"IBLOCKS" => array(
+			0 => "6",
+		),
+		"NEWS_COUNT" => "6",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"DETAIL_URL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "7 776 000",
+		"CACHE_GROUPS" => "Y",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y"
+	),
+	false
+);?>
+
             <h2>Our Services</h2>
           </div>
         </div>
@@ -356,6 +498,33 @@ $APPLICATION->SetTitle("Мебельная компания");
 
   <div class="site-section bg-light">
     <div class="container">
+        <?$APPLICATION->IncludeComponent(
+	"bitrix:news.line", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"IBLOCK_TYPE" => "news",
+		"IBLOCKS" => array(
+			0 => "1",
+		),
+		"NEWS_COUNT" => "3",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"DETAIL_URL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "604 800",
+		"CACHE_GROUPS" => "Y",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y"
+	),
+	false
+);?>
+        
       <div class="row justify-content-center mb-5">
         <div class="col-md-7 text-center">
           <div class="site-section-title">
--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -73,16 +73,49 @@ Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/main.js');
         <div class="row align-items-center">
           <div class="col-6 col-md-6">
             <p class="mb-0">
-              <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+
+                <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/phone.php",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
+                <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/email.php",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
+
+
+
             </p>
           </div>
           <div class="col-6 col-md-6 text-right">
-            <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-            <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-            <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
+              <?$APPLICATION->IncludeComponent(
+                  "bitrix:main.include",
+                  ".default",
+                  array(
+                      "AREA_FILE_SHOW" => "file",
+                      "AREA_FILE_SUFFIX" => "inc",
+                      "EDIT_TEMPLATE" => "",
+                      "PATH" => "/include/social.php",
+                      "COMPONENT_TEMPLATE" => ".default"
+                  ),
+                  false
+              );?>
           </div>
         </div>
       </div>
@@ -92,15 +125,46 @@ Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/main.js');
       <div class="container py-1">
         <div class="row align-items-center">
           <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                    class="text-danger">.</span></strong></a></h1>
+
+              <?$APPLICATION->IncludeComponent(
+                  "bitrix:main.include",
+                  ".default",
+                  array(
+                      "AREA_FILE_SHOW" => "file",
+                      "AREA_FILE_SUFFIX" => "inc",
+                      "EDIT_TEMPLATE" => "",
+                      "PATH" => "/include/logo.php",
+                      "COMPONENT_TEMPLATE" => ".default"
+                  ),
+                  false
+              );?>
+
+
           </div>
           <div class="col-4 col-md-4 col-lg-8">
             <nav class="site-navigation text-right text-md-right" role="navigation">
 
               <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
                   class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
+                <?$APPLICATION->IncludeComponent(
+	"bitrix:menu", 
+	"horizontal_multilevel", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "3",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "360000000",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "Y",
+		"COMPONENT_TEMPLATE" => "horizontal_multilevel"
+	),
+	false
+);?>
               <ul class="site-menu js-clone-nav d-none d-lg-block">
                 <li class="active">
                   <a href="index.html">Home</a>
